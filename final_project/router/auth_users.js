97016f47a923fe1948b50675e00a73a8b35dae38
jonathanlsn <jonathan.lsn32@gmail.com>
2025-08-14 15:17:05 +0200

    Task 7: perform login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,17 +7,49 @@ let users = [];
 
 const isValid = (username)=>{
   const filteredUsers = users.filter(user => user.username === username);
-  return (filteredUsers.length === 0)
+  return (filteredUsers.length === 0);
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+  console.log(users);
+  const filteredUsers = users.filter(user => (user.username === username && user.password === password));
+  return (filteredUsers.length === 1);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  if (username && password) {
+    if (authenticatedUser(username, password)) {
+      const payload = {
+        username: username
+      };
+      // Generate JWT access token
+      let accessToken = jwt.sign(
+        payload, 
+        'access',
+        { expiresIn: 60 * 5 }
+      );
+
+      // Store access token in session
+      req.session.authorization = {accessToken}
+
+      return res.status(200).json({message: `user ${username} logged successfully`});
+    } else {
+      return res.status(401).json({message: "ERROR: wrong username or password"});
+    }
+
+  }
+
+  if (!username) {
+      return res.status(400).json({ message: "ERROR: username not provided" });
+  } 
+  if (!password) {
+      return res.status(400).json({ message: "ERROR: password not provided" });
+  } 
+
 });
 
 // Add a book review
