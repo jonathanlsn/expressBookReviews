b73776a294ed0ef4cf11d0efbb8c674efc1283bb
jonathanlsn <jonathan.lsn32@gmail.com>
2025-08-14 14:08:09 +0200

    Task 4: get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,11 +12,11 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    if (Object.values(books).length > 0) {
-        return res.status(200).json(Object.values(books));
-    } else {
-        return res.status(404).json({message: "No book stored"});
-    }
+  if (Object.values(books).length > 0) {
+      return res.status(200).json(Object.values(books));
+  } else {
+      return res.status(404).json({message: "No book stored"});
+  }
   
 });
 
@@ -45,8 +45,14 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const booksByTitle = Object.values(books).filter((book) => book.title.toLocaleLowerCase() === title.toLocaleLowerCase())
+
+  if (booksByTitle.length > 0 ) {
+    return res.status(200).json(booksByTitle);
+  } else {
+    return res.status(404).json({message: `Books with title ${title} not found`});
+  }
 });
 
 //  Get book review
